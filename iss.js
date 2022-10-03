0fb877a1dda8062ac0776c39d9c03ab10d8edb1b
bb35319013 <63281840+bb35319013@users.noreply.github.com>
2022-10-03 18:05:18 +0900

    10/3 説明半分くらい

--- iss.js
+++ iss.js
@@ -4,76 +4,76 @@ const html = `
   .extendedh { width: 100%; }
   .extendedv { height: 100%; }
   #wrapper {
-    border: 2px solid blue;
-    border-radius: 5px;
-    background-color: rgba(111, 111, 111, 0.5);
-    box-sizing: border-box;
+    border: 2px solid blue;  //罫線
+    border-radius: 5px;  //境界線半径
+    background-color: rgba(111, 111, 111, 0.5);  //背景色
+    box-sizing: border-box;  //ボックスサイズ
   }
   .extendedh body, .extendedh #wrapper { width: 100%; }
   .extendedv body, .extendedv #wrapper { height: 100%; }
   ::-webkit-scrollbar { width: 8px; background: gray; }
   ::-webkit-scrollbar-thumb { border-radius: 4px; background: red; }
 </style>
-<div id="wrapper">
+<div id="wrapper">   //箱の中身
   <h1>The Incredible ISS</h1>
-  <p>Latitude: <span id="lat">-</span></p>
-  <p>Longitude: <span id="lon">-</span></p>
-  <p>Altitude: <span id="alt">-</span>km</p>
+  <p>Latitude: <span id="lat">-</span></p>  //緯度
+  <p>Longitude: <span id="lon">-</span></p>  //経度
+  <p>Altitude: <span id="alt">-</span>km</p>  //高度
   <p>
     <button id="update">Update</button>
     <button id="jump">Jump</button>
     <button id="follow">Follow</button>
     <button id="resize">Resize</button>
   </p>
-</div>
+</div>  //箱の中身終わり
 <script>
-  let lat, lng, alt;
+  let lat, lng, alt;  //変数定義
 
-  const update = () => {
-    return fetch("https://api.wheretheiss.at/v1/satellites/25544").then(r => r.json()).then(data => {
-      lat = data.latitude;
-      lng = data.longitude;
-      alt = data.altitude * 1000; // km -> m
-      document.getElementById("lat").textContent = data.latitude;
-      document.getElementById("lon").textContent = data.longitude;
-      document.getElementById("alt").textContent = data.altitude;
+  const update = () => {  //updateという関数定義(値書き換え禁止)
+    return fetch("https://api.wheretheiss.at/v1/satellites/25544").then(r => r.json()).then(data => {  //非同期通信でサーバー上にあるほしいデータを取ってくる。成功したらr=responseにr.json()を代入、dataに以下を代入
+      lat = data.latitude;  //latに緯度のデータを代入
+      lng = data.longitude;  //lngに経度のデータを代入
+      alt = data.altitude * 1000; // km -> m  //altに単位をkmからmに直して高度のデータを代入
+      document.getElementById("lat").textContent = data.latitude;  //latタグのノードに含まれるテキストを緯度のデータに変換
+      document.getElementById("lon").textContent = data.longitude;  //lonタグのノードに含まれるテキストを経度のデータに変換
+      document.getElementById("alt").textContent = data.altitude;  //altタグのノードに含まれるテキストを高度のデータに変換
     });
   };
 
-  const send = () => {
-    parent.postMessage({ type: "fly", lat, lng, alt }, "*");
+  const send = () => {  //sendという関数定義(値書き換え禁止)
+    parent.postMessage({ type: "fly", lat, lng, alt }, "*");  //iframeの内側から外側(webassembly?)へ送信?
   };
 
-  // document.getElementById("update").addEventListener("click", update);
-  document.getElementById("jump").addEventListener("click", send);
+  // document.getElementById("update").addEventListener("click", update);  //updateボタンをクリックされたらupdate関数の処理を実行
+  document.getElementById("jump").addEventListener("click", send);  //jumpボタンをクリックされたらsend関数の処理を実行
 
-  const updateExtended = e => {
-    if (e && e.horizontally) {
-      document.documentElement.classList.add("extendedh");
-    } else {
-      document.documentElement.classList.remove("extendedh");
+  const updateExtended = e => {  //updateExtendedというeという引数を持つ関数を定義(値書き換え禁止)
+    if (e && e.horizontally) {  //もしeとe.horazontallyが真なら以下を実行
+      document.documentElement.classList.add("extendedh");  //ドキュメントのルート要素にextendedhにクラスを追加する
+    } else {  //違うなら以下を実行
+      document.documentElement.classList.remove("extendedh");  //ドキュメントのルート要素からextendedhクラスを削除する
     }
-    if (e && e.vertically) {
-      document.documentElement.classList.add("extendedv");
-    } else {
-      document.documentElement.classList.remove("extendedv");
+    if (e && e.vertically) {  //もしeとe.verticallyが真なら以下を実行
+      document.documentElement.classList.add("extendedv");  //ドキュメントのルート要素にextendedvクラスを追加する
+    } else {  //違うなら以下を実行
+      document.documentElement.classList.remove("extendedv");  //ドキュメントのルート要素からextendedhクラスを削除する
     }
   };
 
-  addEventListener("message", e => {
-    if (e.source !== parent || !e.data.extended) return;
-    updateExtended(e.data.extended);
+  addEventListener("message", e => {  //postmessageからのデータを受けっとったら、以下を実行
+    if (e.source !== parent || !e.data.extended) return;  //e.sourceとparentが同じ型で値が等しくないか方が異なる、もしくは、e.data.extendedが真ではないなら？以下を実行
+    updateExtended(e.data.extended);  //e.data.extendedを引数としてuppdateExtended関数を実行
   });
 
-  updateExtended(${JSON.stringify(reearth.widget.extended || null)});
-  update();
+  updateExtended(${JSON.stringify(reearth.widget.extended || null)});  //<-ここわからん
+  update();  //update関数の実行
 
-  let timer;
-  document.getElementById("follow").addEventListener("click", (e) => {
-    if (timer) {
-      clearTimeout(timer);
-      timer = undefined;
-      e.currentTarget.textContent = "Follow";
+  let timer;  //変数の定義
+  document.getElementById("follow").addEventListener("click", (e) => {  //followボタンをクリックされたら以下を実行
+    if (timer) {  //もしtimerが真なら以下を実行
+      clearTimeout(timer);  //setTimeoutによるTimer処理を終了させる
+      timer = undefined;  //timerにundefinedを代入
+      e.currentTarget.textContent = "Follow";  //クリックした要素を含むノード内のテキストをFollowに書き換える
       return;
     }
     const cb = () => update().then(() => {
